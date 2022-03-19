c86a1c930b3b216a5918319e5ba576783f12c2bf
Irfan Farooq <irfandominate@gmail.com>
2022-03-19 11:53:38 +0530

    added projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,14 @@
 export const projects = [
   {
+    title: 'Qiswa Travels',
+    description: "Qiswa Travels is a travel agency website. It's a made using wordpress",
+    image: './images/18.png',
+    tags: ["Wordpress"],
+    source: 'http://www.qiswatravels.com/',
+    id: 20,
+
+  },
+  {
     title: 'Task List',
     description: "A simple task list app that allows you to add, edit, and delete tasks.",
     image: './images/1.png',
@@ -17,6 +26,141 @@ export const projects = [
     source: 'https://loancalculatorbyirfan.netlify.app/',
     visit: 'https://github.com/irfan378/LoanCalculator',
     id: 1,
+  },
+  {
+    title: 'Number guessing game',
+    description: "A simple number guessing game that allows you to guess the number.",
+    image: './images/3.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://numberguessinggamebyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Number-Guessing-Game',
+    id: 2,
+  },
+  {
+    title: 'Libary app',
+    description: "A simple libary app that allows you to add books",
+    image: './images/4.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://libraryappbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Library-Website',
+    id: 3,
+  },
+  {
+    title: 'Form Validator',
+    description: "A simple form validator app that allows you to validate the form.",
+    image: './images/5.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://formvalidationbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Form-Validation',
+    id: 4,
+  },
+  {
+    title: 'CV Screener',
+    description: "A simple cv screener app that allows you to check cv's for recruitment.",
+    image: './images/6.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://cvscreenerbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/CV-Screener',
+    id: 5,
+  },
+  {
+    title: 'Top 10 Batsman in tests',
+    description: "A simple top 10 batsman app that allows you to check top 10 batsman in tests.",
+    image: './images/7.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://top10batsmanbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Drag-and-Drop',
+    id: 6,
+  },
+  {
+    title: 'Text Utils',
+    description: "A simple text utils app that allows you to check text utils.",
+    image: './images/8.png',
+    tags: ['Bootstrap', 'React js'],
+    source: 'https://textutilsbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Text-Utils',
+    id: 7,
+  },
+  {
+    title: 'Single page Food website',
+    description: "A simple landing page for a food company",
+    image: './images/9.png',
+    tags: ['HTML', 'CSS'],
+    source: 'https://singlepagefoodwebsite.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/Single%20page%20food%20website',
+    id: 8,
+  },
+  {
+    title: 'Postman Clone',
+    description: "A simple clone of postman",
+    image: './images/10.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://postmanclonebyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Postman-Clone',
+    id: 9
+  },
+  {
+    title: 'Animated Car',
+    description: "A simple animated car",
+    image: './images/11.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://animatedcarbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/Car%20Aminated',
+    id: 10
+  },
+  {
+    title: 'Clock ',
+    description: "A simple clock",
+    image: './images/12.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://clockbyirfanfarooq.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/clock%20using%20html%20css%20and%20javascript',
+    id: 11
+  },
+  {
+    title: 'Corona Virus Plotting Map ',
+    description: "A simple plotting map of corona virus",
+    image: './images/13.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://coronavirusplottingmap.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/Corona%20Virus%20plotting%20map',
+    id: 12
+  },
+  {
+    title: 'Dragon game',
+    description: "A simple game of dragon which is played in browser",
+    image: './images/14.png',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://dragon-gamebyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/JavaScript%20Dragon%20Game',
+    id: 13
+  },
+  {
+    title: 'Responsive Blog',
+    description: "A simple responsive blog made using Html and css",
+    image: './images/15.png',
+    tags: ['HTML', 'CSS'],
+    source: 'https://responsiveblogbyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/responsive%20blog',
+    id: 14
+  },
+  {
+    title: 'Snake game',
+    description: "A simple game of snake which is played in browser",
+    image: './images/16.png',
+    tags: ['HTML', 'CSS', "JavaScript"],
+    source: 'https://snakegamebyirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/SnakeGame',
+    id: 15
+  },
+  {
+    title: 'Windows 11',
+    description: "A simple UI of Windows 11",
+    image: './images/17.png',
+    tags: ['HTML', 'CSS', "JavaScript"],
+    source: 'https://windows11byirfan.netlify.app/',
+    visit: 'https://github.com/irfan378/Frontend-Development/tree/main/html%20css%20js/windows%2011',
+    id: 16
   },
 
 ];
